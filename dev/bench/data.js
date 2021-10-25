71af74c73208c462627754acc37a3867e0a2d2de
github-action-benchmark <github@users.noreply.github.com>
2021-10-25 23:34:57 +0000

    add Benchmark (pytest) benchmark result for 726e652b08d177d95d2e1132569ef9ba06acaf72

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1635204897160,
+  "repoUrl": "https://github.com/wesleyjia34/cpt-demo",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wesley.jia@placeexchange.com",
+            "name": "Wesley Jia",
+            "username": "wesleyjia34"
+          },
+          "committer": {
+            "email": "wesley.jia@placeexchange.com",
+            "name": "Wesley Jia",
+            "username": "wesleyjia34"
+          },
+          "distinct": true,
+          "id": "726e652b08d177d95d2e1132569ef9ba06acaf72",
+          "message": "adding benchmarking",
+          "timestamp": "2021-10-25T19:34:24-04:00",
+          "tree_id": "3f1a125446d9f9167f77267bf363a5fd0aed80d4",
+          "url": "https://github.com/wesleyjia34/cpt-demo/commit/726e652b08d177d95d2e1132569ef9ba06acaf72"
+        },
+        "date": 1635204895891,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "src/demo/tests/test_utils.py::test_benchmark_looper_time",
+            "value": 8.113159880399557,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00018860277858608653",
+            "extra": "mean: 123.25653811111043 msec\nrounds: 9"
+          }
+        ]
+      }
+    ]
+  }
+}
