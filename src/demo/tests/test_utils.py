726e652b08d177d95d2e1132569ef9ba06acaf72
Wesley Jia <wesley.jia@placeexchange.com>
2021-10-25 19:34:24 -0400

    adding benchmarking

--- src/demo/tests/test_utils.py
+++ src/demo/tests/test_utils.py
@@ -5,5 +5,5 @@ def test_looper_returns_iter_count():
     completed_iters = looper(input_iters)
     assert completed_iters == 1000000
 
-def test_looper_time(benchmark):
+def test_benchmark_looper_time(benchmark):
     benchmark(looper, 1000000)
