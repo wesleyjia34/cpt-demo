b840e4a4c0987d89456f64a12adbbd35f6b2b653
Wesley Jia <wesley.jia@placeexchange.com>
2021-10-25 21:21:36 -0400

    added second benchmark test

--- src/demo/tests/test_utils.py
+++ src/demo/tests/test_utils.py
@@ -1,4 +1,4 @@
-from ..utils import looper
+from ..utils import looper, reduce_fibonacci_nums
 
 def test_looper_returns_iter_count():
     input_iters = 1000000
@@ -7,3 +7,6 @@ def test_looper_returns_iter_count():
 
 def test_benchmark_looper_time(benchmark):
     benchmark(looper, 1000000)
+
+def test_benchmark_reduce_fib_nums_time(benchmark):
+    benchmark(reduce_fibonacci_nums)
--- src/demo/utils.py
+++ src/demo/utils.py
@@ -1,6 +1,12 @@
-def looper(iter_count: int):
+from functools import reduce
+
+def looper(iter_count: int) -> int:
     for i in range(iter_count):
         for j in range(25):
             i
     return i + 1
     
+def reduce_fibonacci_nums() -> int:
+    
+    fib_num_count = 10
+    return reduce(lambda x, y: x + y, range(fib_num_count))
