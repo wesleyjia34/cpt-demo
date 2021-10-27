26bb1a0fd57b4e3d5028ef264f6b3fa0186dc30a
Wesley Jia <wesley.jia@placeexchange.com>
2021-10-26 23:46:34 -0400

    sum more ints

--- src/demo/utils.py
+++ src/demo/utils.py
@@ -8,6 +8,6 @@ def looper(iter_count: int) -> int:
     
 def sum_incrementing_ints() -> int:
     
-    max_int = 10
+    max_int = 50
     return reduce(lambda x, y: x + y, range(1, max_int + 1))
 g
